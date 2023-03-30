0f06405ddf6aa9f7b56df6a3a852881de276c81e
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-03-30 13:57:12 +0500

    Min-Max

--- HomeWork_6/HomeWork_6/Program.cs
+++ HomeWork_6/HomeWork_6/Program.cs
@@ -10,6 +10,7 @@ namespace HomeWork_6
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("Enter Number : ");
             string str = Console.ReadLine();
             bool check = int.TryParse(str, out int number);
@@ -19,6 +20,28 @@ namespace HomeWork_6
                 factorial *= i;
             }
             Console.WriteLine($"The factorial value of the number {number} is equal to {factorial} ");
+=======
+            Console.Write("Enter Number1 : ");
+            int Number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number2 : ");
+            int Number2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number3 : ");
+            int Number3 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number4 : ");
+            int Number4 = int.Parse(Console.ReadLine());
+            int max = Number1;
+            int min = Number1;
+            if (max < Number2) max = Number2;
+            if (max < Number3) max = Number3;
+            if (max < Number4) max = Number4;
+            if (min > Number2) min = Number2;
+            if (min > Number3) min = Number3;
+            if (min > Number4) min = Number4;
+            Console.WriteLine($"Max : {max}");
+            Console.WriteLine($"Min : {min}");
+            Console.WriteLine($"Result : {max - min}");
+            Main(args);
+>>>>>>> Stashed changes
         }
     }
 }
